ad813274b30b538b6a817c00d26b492b98c8c4f8
Filipe Kiss <hello@filipekiss.com.br>
2022-04-20 16:19:20 +0200

    feat(commands): Remove commands

--- commands.lua
+++
@@ -1,6 +0,0 @@
-local add_command = require("nebula.helpers.nvim").add_user_command
-
-local neovim_config = vim.fn.stdpath("config")
-add_command("EditInit", string.format("edit %s", neovim_config .. "/init.lua"))
-
-add_command("ClearSearch", "nohlsearch")
